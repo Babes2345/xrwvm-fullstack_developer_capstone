df11aae26806aecfeee460d6fc8af33ef24c81ec
Babes2345 <babesandrew@gmail.com>
2024-04-17 11:45:20 -0500

    Updates

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -12,27 +12,25 @@ def get_request(endpoint, **kwargs):
     params = ""
     if kwargs:
         for key, value in kwargs.items():
-            params = params + key + "=" + value + "&"
+            params += key + "=" + value + "&"
 
     request_url = backend_url + endpoint + "?" + params
 
     print("GET from {}".format(request_url))
     try:
-        # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except Exception as err:  # Use a more specific exception
-        print(f"Network exception occurred: {err}")
+    except Exception as e:
+        print(f"Network exception occurred: {e}")
 
 
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url + "analyze/" + text
     try:
-        # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except Exception as err:
-        print(f"Unexpected {err=}, {type(err)=}")
+    except Exception as e:
+        print(f"Unexpected {e=}, {type(e)=}")
         print("Network exception occurred")
 
 
@@ -40,7 +38,8 @@ def post_review(data_dict):
     request_url = backend_url + "/insert_review"
     try:
         response = requests.post(request_url, json=data_dict)
-        print(response.json())
         return response.json()
-    except Exception as err:  # Specify the exception
-        print(f"Network exception occurred: {err}")
+    except Exception as e:
+        print(f"Network exception occurred: {e}")
+
+# Ensuring newline at the end of the file
